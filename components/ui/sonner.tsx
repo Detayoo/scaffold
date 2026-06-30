56e40834ea6e215e1902310fe7fce39c96ee10a0
detayoo <teeydigba@gmail.com>
2026-06-30 16:01:28 +0100

    fix: move toasts to bottom-right

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -9,7 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
-      position="bottom-left"
+      position="bottom-right"
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
