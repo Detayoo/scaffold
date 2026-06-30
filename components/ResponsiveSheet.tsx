abdcba264ddd8cfdbe38ba81caab0570e028801e
detayoo <teeydigba@gmail.com>
2026-06-30 20:53:39 +0100

    fix: add bottom padding to sheet scroll area

--- components/ResponsiveSheet.tsx
+++ components/ResponsiveSheet.tsx
@@ -74,7 +74,7 @@ export function ResponsiveSheet({
             )}
           </SheetHeader>
         )}
-        <ScrollArea className="flex-1 min-h-0 px-4">
+        <ScrollArea className="flex-1 min-h-0 px-4 pb-6">
           {children}
         </ScrollArea>
       </SheetContent>
