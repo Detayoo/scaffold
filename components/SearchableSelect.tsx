3cd82a6ddab6910aa7c7cc52fedafd6cb38b8409
detayoo <teeydigba@gmail.com>
2026-07-01 12:24:20 +0100

    fix(ui): add max-h and overflow to SearchableSelect popover

--- components/SearchableSelect.tsx
+++ components/SearchableSelect.tsx
@@ -71,7 +71,7 @@ export function SearchableSelect({
           <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="p-0" style={{ width: triggerWidth || undefined }}>
+      <PopoverContent className="p-0 max-h-80 overflow-y-auto" style={{ width: triggerWidth || undefined }}>
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
           <CommandList>
